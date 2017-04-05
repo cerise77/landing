27f20853d11fc14e20cc07025b738b54bc53410e
cerise77 <vanass567@gmail.com>
2017-04-05 15:43:48 +0300

    ter

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+/*const http = require('http');
+let router = require('./server/server2');
+const hostname = '127.0.0.1';
+const port = 3000;
+let info = "\nnode-static-http-server by Janus Nic\nExamples of HTTP static file serving in Node.js\nSee: https://github.com/janusnic/web-dev-node\n";
+
+const server = http.createServer(router.handleRequest);
+server.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}/`);
+});
+console.log(info);*/
+
+
+//////////////////////////////////////
+
+const Server = require('./server/server4');
+//
+const hostname = '127.0.0.1';
+const port = 3000;
+let info = "\nnode-static-http-server by Janus Nic\nExamples of HTTP static file serving in Node.js\nSee: https://github.com/janusnic/web-dev-node\n";
+Server.start(port, hostname, info);
