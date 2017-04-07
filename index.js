1ac1967162f9aba6547a888e8c5b19052366a98d
cerise77 <vanass567@gmail.com>
2017-04-07 13:41:27 +0300

    rr

--- index.js
+++ index.js
@@ -14,8 +14,14 @@ console.log(info);*/
 //////////////////////////////////////
 
 const Server = require('./server/server4');
-//
-const hostname = '127.0.0.1';
-const port = 3000;
-let info = "\nnode-static-http-server by Janus Nic\nExamples of HTTP static file serving in Node.js\nSee: https://github.com/janusnic/web-dev-node\n";
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+//const hostname = '127.0.0.1';
+//const port = 3000;
+//let info = "\nnode-static-http-server by Janus Nic\nExamples of HTTP static file serving in Node.js\nSee: https://github.com/janusnic/web-dev-node\n";
 Server.start(port, hostname, info);
