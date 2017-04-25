c1b1e6e339573a001cf2c419720d12ab5d1f523d
cerise77 <vanass567@gmail.com>
2017-04-25 17:11:28 +0300

    fvmgk

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/blog');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
