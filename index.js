f1fe7a8357cd85d07819935df4d2723a0d2f98dc
cerise77 <vanass567@gmail.com>
2017-05-05 12:13:50 +0300

    fasder

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-/*const http = require('http');
-let router = require('./server/server2');
-const hostname = '127.0.0.1';
-const port = 3000;
-let info = "\nnode-static-http-server by Janus Nic\nExamples of HTTP static file serving in Node.js\nSee: https://github.com/janusnic/web-dev-node\n";
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+var port  	 = process.env.PORT || 5000; 				// set the port
+//var database = require('./config/database'); 			// load the database config
+var morgan   = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-const server = http.createServer(router.handleRequest);
-server.listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
+// configuration ===============================================================
+//mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+
+
+// routes ======================================================================
+require('./app/routes.js')(app);
+
+// listen (start app with node server.js) ======================================
+// app.listen(port);
+// console.log("App listening on port " + port);
+
+// app.get('/', function(request, response) {
+//  response.send('Hello World!');
+//});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
-console.log(info);*/
-
-
-//////////////////////////////////////
-
-const Server = require('./server/server4');
-//
-const hostname = '127.0.0.1';
-const port = 5000;
-//const info = require('web-dev-node:serve');
-let info = "\nnode-static-http-server by cerise77\nExamples of HTTP static file serving in Node.js\nSee: https://github.com/cerise77/plan";
-//let info = "\nnode-static-http-server by Janus Nic\nExamples of HTTP static file serving in Node.js\nSee: https://github.com/janusnic/web-dev-node\n";
-Server.start(port, hostname, info);
